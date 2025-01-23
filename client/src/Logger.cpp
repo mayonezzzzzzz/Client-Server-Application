0c2ded9c592d19ff505928fa46876fb8778cdc19
Ilya <poliakov.ilia.outlook.com@gmail.com>
2025-01-23 17:10:00 +0300

    feat(client): adding thread ID output using boost::format

--- client/src/Logger.cpp
+++ client/src/Logger.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <ctime>
 #include <iomanip>
+#include <thread>
+#include <boost/format.hpp>
 
 // Функция для логирования событий в файл client.log внутри папки с проектом
 void log(const std::string& level, const std::string& message) {
@@ -9,7 +11,14 @@ void log(const std::string& level, const std::string& message) {
     if (log_file.is_open()) {
         time_t now = time(0);
         std::tm local_time = *std::localtime(&now);
-        log_file << "[" << std::put_time(&local_time, "%Y-%m-%d %H:%M:%S") << "] [" << level << "] " << message << "\n";
+        auto thread_id = std::this_thread::get_id();
+
+        std::string log = boost::str(
+            boost::format("[%1%]"     " [Thread: %2%]"     " [%3%]"     " %4%")
+            % std::put_time(&local_time, "%Y-%m-%d %H:%M:%S")
+            % thread_id % level % message
+        );
+        //log_file << "[" << std::put_time(&local_time, "%Y-%m-%d %H:%M:%S") << "] [" << level << "] " << message << "\n";
     }
 }
 
