32e2d1b025495cd8fae45be27ee4374b998e17dd
Ilya <poliakov.ilia.outlook.com@gmail.com>
2025-01-23 17:16:58 +0300

    feat(server): finding number of cores in processor

--- server/src/HTTP_Server.cpp
+++ server/src/HTTP_Server.cpp
@@ -9,24 +9,23 @@ using tcp = asio::ip::tcp;
 
 int main() {
     try {
-        auto ioc = std::make_shared<asio::io_context>(6);
+        const auto& numOfProcessors = std::thread::hardware_concurrency();
+        auto ioc = std::make_shared<asio::io_context>(numOfProcessors);
 
         auto message_handler = std::make_shared<MessageHandler>(ioc);
         message_handler->startHandle();
 
         // Пул потоков
         std::vector<std::thread> threads;
-        threads.reserve(6);
-        for (std::size_t i = 0; i < 6; ++i) {
+        threads.reserve(numOfProcessors);
+        for (std::size_t i = 0; i < numOfProcessors; ++i) {
             threads.emplace_back([ioc]() {
                 ioc->run(); // Каждый поток запускает обработку задач в io_context
                 });
         }
 
         for (auto& thread : threads) {
-            if (thread.joinable()) {
-                thread.join();
-            }
+            thread.join();
         }
     }
     catch (boost::system::system_error& err) {
